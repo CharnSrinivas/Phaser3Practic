24761c84b68a6c08ef09781cfb5e42687ea51636
charan srinivas <charansrinivassajja@gmail.com>
2021-10-31 14:42:48 +0530

    Initial

--- src/Scenes/MainScene/index.ts
+++ src/Scenes/MainScene/index.ts
@@ -1,30 +1,56 @@
 import * as Phaser from "phaser";
+import Player from '../../Objects/Player';
 // import {} from '../../assets/Levels/level_1.json'
 
 export default class extends Phaser.Scene {
-    // constructor() {
-    //     super("main-screen")        
-    // }
-
+    level:number[][]|undefined;
+    player:Player | undefined = new Player();
+    private cursor?:Phaser.Types.Input.Keyboard.CursorKeys;
     preload() {
         
         console.log('preload');
         
-        this.load.tilemapTiledJSON('map', 'assets/Levels/level_1.json')
+        
         this.load.spritesheet('tiles', ('assets/tilesheet.png'), 
             {
                 frameWidth: 128,
                 startFrame: 0
             }
         )
-        // this.load.image('tiles','assets/tilesheet.png');
     }
     create() {
-        const map = this.make.tilemap({ key: 'map' });
-        const tiles = map.addTilesetImage('sokobon','tiles');
-        console.log(tiles);
+        this.level=[
+            [100 ,100,  100,  100 , 100, 100 ,100 , 100 , 100 ,100] ,
+            [100 , 0   , 0 ,  0 ,   0,    0,   0,   0,    0,   100],
+            [100 , 0 ,   0 ,  0 ,  0,    0,   0,   0,    0,   100],
+            [100 , 0 ,   0 ,  0 ,   0,    0,   0,   0,    0,   100],
+            [100 , 0 ,   0 ,  0 ,   0,    0,   0,   0,    0,   100],
+            [100 , 0 ,   0 ,  0 ,   0,    0,   0,   0,    0,   100],
+            [100 , 0 ,   0 ,  0 ,   0,    0,   0,   0,    52,   100],
+            [100 ,100 , 100 , 100 , 100, 100, 100, 100,   100,   100],
+        ]
+        const map = this.make.tilemap({data:this.level,tileHeight:128,tileWidth:128});
+        const tiles = map.addTilesetImage('tiles');
+        const layer = map.createLayer(0,tiles);
+        const player  = layer.createFromTiles(52,0,{ frame:52,key:'tiles'}).pop();
+        this.cursor = this.input.keyboard.createCursorKeys();
+        if (player)
+            this.player?.createPlayer(player);        
         
-        const layer = map.createLayer('my_layer', tiles, 0, 0);
-
+    }
+    update(){
+        if(this.cursor?.left.isDown){
+            this.player?.moveLeft();
+        }
+        else if(this.cursor?.right.isDown){
+            this.player?.moveRight();
+        }
+        else if(this.cursor?.up.isDown){
+            this.player?.moveUp();
+        }
+        else if(this.cursor?.down.isDown){
+            this.player?.moveDown();
+        }
+        else{this.player?.makeIdle()}
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import {Types,Game} from 'phaser';
 import Scene from './Scenes/MainScene';
 export const config:Types.Core.GameConfig = {                                
     type: Phaser.AUTO,
-    width: 20*64,// ? Width of tile sheet X tile width
-    height: 20*64,// ? Height of tile sheet X tile height
+    width: 1280,
+    height: 1024,
     physics:{default:'arcade',arcade:{gravity:{y:10},debug:true}
 }
 };
